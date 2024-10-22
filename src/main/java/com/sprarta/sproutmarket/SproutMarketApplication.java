bbed81b8822a771fad913cf6acfa60cc68e0b131
durururuk <147094944+durururuk@users.noreply.github.com>
2024-10-22 10:01:38 +0900

    git init

---
+++ src/main/java/com/sprarta/sproutmarket/SproutMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sprarta.sproutmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SproutMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprarta/sproutmarket/SproutMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprarta.sproutmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
