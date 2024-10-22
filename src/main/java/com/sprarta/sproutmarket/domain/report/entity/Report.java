49b69e409a64b9df2179318740b6ad153fd3c44e
durururuk <147094944+durururuk@users.noreply.github.com>
2024-10-22 11:40:57 +0900

    fix : reportingReason 스네이크 케이스 -> 카멜케이스
    Report Entity의 reporting_reason을 reportingReason으로 변경했습니다.

--- src/main/java/com/sprarta/sproutmarket/domain/report/entity/Report.java
+++ src/main/java/com/sprarta/sproutmarket/domain/report/entity/Report.java
@@ -19,7 +19,7 @@ public class Report {
     private Long id;
 
     @Column(nullable = false)
-    private String reporting_reason;
+    private String reportingReason;
 
     @Enumerated(EnumType.STRING)
     private ReportStatus reportStatus;
