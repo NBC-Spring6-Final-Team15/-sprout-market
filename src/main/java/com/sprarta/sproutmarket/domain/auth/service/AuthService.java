6ef658b1ec3a6570ce4b79a08949f8af1de0f187
kikye04040 <kikye04040@gmail.com>
2024-10-22 14:54:02 +0900

    feat(user): 회원가입/로그인 구현
    build(user): DB 오류 수정을 위해 properties, build 수정
    fix(user): 엔티티 생성자 추가
    feat(user): auth service, controller, dto 구현

---
+++ src/main/java/com/sprarta/sproutmarket/domain/auth/service/AuthService.java
@@ -0,0 +1,63 @@
+package com.sprarta.sproutmarket.domain.auth.service;
+
+import com.sprarta.sproutmarket.config.JwtUtil;
+import com.sprarta.sproutmarket.domain.auth.dto.request.SigninRequest;
+import com.sprarta.sproutmarket.domain.auth.dto.request.SignupRequest;
+import com.sprarta.sproutmarket.domain.auth.dto.response.SigninResponse;
+import com.sprarta.sproutmarket.domain.auth.dto.response.SignupResponse;
+import com.sprarta.sproutmarket.domain.user.entity.User;
+import com.sprarta.sproutmarket.domain.user.enums.UserRole;
+import com.sprarta.sproutmarket.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public SignupResponse signup(SignupRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        UserRole userRole = UserRole.of(request.getUserRole());
+
+        User newUser = new User(
+                request.getUsername(),
+                request.getEmail(),
+                encodedPassword,
+                request.getNickname(),
+                request.getPhoneNumber(),
+                request.getAddress(),
+                userRole
+        );
+        User savedUser = userRepository.save(newUser);
+
+        String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
+
+        return new SignupResponse(bearerToken);
+    }
+
+    public SigninResponse signin(SigninRequest request) {
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new IllegalArgumentException("가입되지 않은 유저입니다."));
+
+        // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getUserRole());
+
+        return new SigninResponse(bearerToken);
+    }
+}
