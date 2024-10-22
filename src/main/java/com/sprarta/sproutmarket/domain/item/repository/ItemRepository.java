4713670f3a3b03d23f7e81b79a8ce8a9f08281dc
durururuk <147094944+durururuk@users.noreply.github.com>
2024-10-23 00:51:50 +0900

    Fix : ItemRepository 쿼리메서드가 잘못 작성돼있던 문제 수정
    Item 안의 User 객체명이 sellerId 라서 쿼리메서드가 작동하지 않아 서버 실행이 안 되던 문제를 일단 JPQL 로 수정했습니다.
    필드명 논의 후 수정 예정

--- src/main/java/com/sprarta/sproutmarket/domain/item/repository/ItemRepository.java
+++ src/main/java/com/sprarta/sproutmarket/domain/item/repository/ItemRepository.java
@@ -2,10 +2,12 @@ package com.sprarta.sproutmarket.domain.item.repository;
 
 import com.sprarta.sproutmarket.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-    Optional<Item> findByIdAndSellerId(Long itemId, Long sellerId);
+    @Query("SELECT i FROM Item i WHERE i.id = :itemId AND i.sellerId.id = :sellerId")
+    Optional<Item> findByIdAndSellerId(@Param("itemId") Long itemId, @Param("sellerId") Long sellerId);
 }
