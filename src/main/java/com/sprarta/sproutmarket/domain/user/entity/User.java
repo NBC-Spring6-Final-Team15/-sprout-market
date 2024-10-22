65dbad5aa2fb38b6cf5ed86836ce2ea89d8685c3
kikye04040 <kikye04040@gmail.com>
2024-10-22 11:09:13 +0900

    feat(user): entity, repository 생성

---
+++ src/main/java/com/sprarta/sproutmarket/domain/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.sprarta.sproutmarket.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_address_id")
+    private ActivityArea activityAddress;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Report> reports;
+}
