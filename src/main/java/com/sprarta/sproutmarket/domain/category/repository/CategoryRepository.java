0c60c23857911390ffb5eab681ee3cacc3c8b026
Hyemin Yang <asitwas729@gmail.com>
2024-10-22 11:38:28 +0900

    init : item, image, category repository 생성

---
+++ src/main/java/com/sprarta/sproutmarket/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.sprarta.sproutmarket.domain.category.repository;
+
+import com.sprarta.sproutmarket.domain.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
