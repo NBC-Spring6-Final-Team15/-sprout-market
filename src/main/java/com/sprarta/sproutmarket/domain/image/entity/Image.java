6170115cdf8030553b28d6de828d0ceeb8b56324
Hyemin Yang <asitwas729@gmail.com>
2024-10-22 11:19:11 +0900

    init : category, item, image entity 기본 셋팅

---
+++ src/main/java/com/sprarta/sproutmarket/domain/image/entity/Image.java
@@ -0,0 +1,19 @@
+package com.sprarta.sproutmarket.domain.image.entity;
+
+import com.sprarta.sproutmarket.domain.item.entity.Item;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item_id;
+}
