27f2978c4a5817732d31cadffa914854781ae03b
EtherXion <etel0103@gmail.com>
2024-10-24 19:49:33 +0900

    fix : withSockJS 삭제

--- src/main/java/com/sprarta/sproutmarket/config/WebSocketConfig.java
+++ src/main/java/com/sprarta/sproutmarket/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.sprarta.sproutmarket.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry; // 메시지 브로커 구성 정의
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry; // STOMP 프로토콜의 엔드포인트 등록
@@ -18,8 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         // 웹소켓이 연결하는 endpoint
         stompEndpointRegistry.addEndpoint("/ws") // 연결 url 설정
-                .setAllowedOriginPatterns("*") // 모든 출처에서의 요청을 허용 이후 보안상 변경할 것
-                .withSockJS(); // SockJS를 사용하여 브라우저에서 웹소켓을 지원하지 않는 경우에도 대체 프로토콜로 연결
+                .setAllowedOriginPatterns("*"); // 모든 출처에서의 요청을 허용 이후 보안상 변경할 것
 
     }
 
