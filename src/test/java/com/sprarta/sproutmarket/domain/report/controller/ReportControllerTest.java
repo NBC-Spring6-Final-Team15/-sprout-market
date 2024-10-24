57c9f1c27c7276c0e6c373d2075bb48bd146be4c
durururuk <147094944+durururuk@users.noreply.github.com>
2024-10-24 23:48:05 +0900

    Feat : 신고 컨트롤러 테스트 - 신고 단건,다건,수정,삭제 작성
    특정 신고 ID로 신고를 단건조회하는 기능,
    특정 매물 ID로 신고를 다건조회하는 기능,
    특정 신고 ID로 신고 수정하는 기능,
    특정 신고 ID로 신고 삭제하는 기능에 대하여 컨트롤러 테스트 코드를 작성했습니다.

--- src/test/java/com/sprarta/sproutmarket/domain/report/controller/ReportControllerTest.java
+++ src/test/java/com/sprarta/sproutmarket/domain/report/controller/ReportControllerTest.java
@@ -14,6 +14,7 @@ import com.sprarta.sproutmarket.domain.user.entity.CustomUserDetails;
 import com.sprarta.sproutmarket.domain.user.entity.User;
 import com.sprarta.sproutmarket.domain.user.enums.UserRole;
 import com.sprarta.sproutmarket.domain.user.service.CustomUserDetailService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,10 +32,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.epages.restdocs.apispec.ResourceDocumentation.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,25 +50,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc(addFilters = false)
 class ReportControllerTest {
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
 
     @MockBean
-    private ReportService reportService;
+    ReportService reportService;
 
     @MockBean
-    private JwtUtil jwtUtil;
+    JwtUtil jwtUtil;
 
     @MockBean
-    private CustomUserDetailService customUserDetailService;
+    CustomUserDetailService customUserDetailService;
 
     @MockBean
     JpaMetamodelMappingContext jpaMappingContext;
 
     @MockBean
-    private CustomUserDetails mockAuthUser;
+    CustomUserDetails mockAuthUser;
+
+    Long itemId;
+    String reportingReason;
+    ReportRequestDto requestDto;
+    ReportResponseDto responseDto;
 
     @BeforeEach
     void setUp() {
@@ -74,16 +83,17 @@ class ReportControllerTest {
         // 인증 유저 스프링 컨텍스트 홀더에 저장
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(mockAuthUser, null, mockAuthUser.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        itemId = 1L;
+        reportingReason = "지우개를 5만원에 팔아요";
+        requestDto = new ReportRequestDto(reportingReason);
+        responseDto = new ReportResponseDto(1L, 1L, reportingReason, ReportStatus.WAITING);
     }
 
     @Test
     @WithMockUser
     void createReport() throws Exception {
         //given
-        Long itemId = 1L;
-        String reportingReason = "지우개를 5만원에 팔아요";
-        ReportRequestDto requestDto = new ReportRequestDto(reportingReason);
-        ReportResponseDto responseDto = new ReportResponseDto(1L, 1L, reportingReason, ReportStatus.WAITING);
         when(reportService.createReport(anyLong(), any(ReportRequestDto.class), any(CustomUserDetails.class)))
                 .thenReturn(responseDto);
 
@@ -94,7 +104,7 @@ class ReportControllerTest {
                         .header("Authorization", "Bearer (JWT 토큰)"))
                 .andDo(MockMvcRestDocumentationWrapper.document("create-report",
                         resource(ResourceSnippetParameters.builder()
-                                .description("특정 매물 ID에 대한 신고를 생성합니다.")
+                                .description("특정 매물에 대한 신고를 생성합니다.")
                                 .summary("매물 신고 생성")
                                 .tag("report")
                                 .pathParameters(
@@ -128,9 +138,7 @@ class ReportControllerTest {
                                 .requestSchema(Schema.schema("신고-생성-성공-요청"))
                                 .responseSchema(Schema.schema("신고-생성-성공-응답"))
                                 .build()
-                        )
-                )
-                );
+                        )));
 
         //then
         result.andExpect(status().isOk())
@@ -139,18 +147,195 @@ class ReportControllerTest {
     }
 
     @Test
-    void getReport() {
+    @WithMockUser
+    void getReport() throws Exception {
+        Long reportId = 1L;
+        when(reportService.getReport(anyLong())).thenReturn(responseDto);
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.get("/reports/{reportId}", reportId)
+                .header("Authorization", "Bearer (JWT 토큰)"))
+                .andDo(MockMvcRestDocumentationWrapper.document(
+                        "get-a-report",
+                        resource(ResourceSnippetParameters.builder()
+                                .description("신고 ID로 신고를 단건조회합니다.")
+                                .summary("신고 단건 조회")
+                                .tag("report")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("Bearer (JWT 토큰)")
+                                )
+                                .pathParameters(
+                                        parameterWithName("reportId").description("조회할 신고 ID")
+                                )
+                                .responseFields(
+                                        fieldWithPath("message")
+                                                .description("성공 메시지 : Ok"),
+                                        fieldWithPath("statusCode")
+                                                .description("성공 상태 코드 : 200"),
+                                        fieldWithPath("data")
+                                                .description("본문 응답"),
+                                        fieldWithPath("data.id")
+                                                .description("신고 ID"),
+                                        fieldWithPath("data.itemId")
+                                                .description("신고된 매물 ID"),
+                                        fieldWithPath("data.reportingReason")
+                                                .description("신고된 사유"),
+                                        fieldWithPath("data.reportStatus")
+                                                .description("신고 상태 : 기본값 WAITING")
+                                )
+                                .responseSchema(Schema.schema("신고-단건-조회-성공-응답"))
+                                .build()
+
+                        )));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.statusCode").value(200));
+        result.andExpect(jsonPath("$.data.reportingReason").value(reportingReason));
     }
 
     @Test
-    void getReports() {
+    @WithMockUser
+    void getReports() throws Exception {
+        ReportResponseDto responseDto2 = new ReportResponseDto(2L, 1L, "직거래 약속 잡았는데 안 나와요", ReportStatus.WAITING);
+        List<ReportResponseDto> responseDtoList = new ArrayList<>();
+        responseDtoList.add(responseDto);
+        responseDtoList.add(responseDto2);
+        when(reportService.getReports(anyLong())).thenReturn(responseDtoList);
+
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.get("/items/{itemId}/reports",itemId)
+                .header("Authorization", "Bearer (JWT 토큰)"))
+                .andDo(MockMvcRestDocumentationWrapper.document(
+                        "get-reports",
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .description("특정 매물에 대한 신고 다건 조회")
+                                        .summary("특정 Item 신고 다건 조회")
+                                        .tag("report")
+                                        .requestHeaders(
+                                                headerWithName("Authorization")
+                                                        .description("Bearer (JWT 토큰)")
+                                        )
+                                        .pathParameters(
+                                                parameterWithName("itemId").description("조회할 매물 ID")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("message")
+                                                        .description("성공 메시지 : Ok"),
+                                                fieldWithPath("statusCode")
+                                                        .description("성공 상태 코드 : 200"),
+                                                fieldWithPath("data")
+                                                        .description("본문 응답"),
+                                                fieldWithPath("data[].id")
+                                                        .description("신고 ID"),
+                                                fieldWithPath("data[].itemId")
+                                                        .description("신고된 매물 ID"),
+                                                fieldWithPath("data[].reportingReason")
+                                                        .description("신고된 사유"),
+                                                fieldWithPath("data[].reportStatus")
+                                                        .description("신고 상태 : 기본값 WAITING")
+                                        )
+                                        .responseSchema(Schema.schema("특정-매물-신고-다건-조회-성공-응답"))
+                                        .build()
+                        )));
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", Matchers.hasSize(2)));
     }
 
     @Test
-    void updateReport() {
+    @WithMockUser
+    void updateReport() throws Exception {
+        //given
+        Long reportId = 1L;
+        when(reportService.updateReport(anyLong(), any(ReportRequestDto.class), any(CustomUserDetails.class)))
+                .thenReturn(responseDto);
+
+        //when
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.put("/reports/{reportId}", reportId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .header("Authorization", "Bearer (JWT 토큰)"))
+                .andDo(MockMvcRestDocumentationWrapper.document("update-report",
+                        resource(ResourceSnippetParameters.builder()
+                                .description("신고 ID와 수정할 신고사유를 받아서 신고를 수정합니다.")
+                                .summary("매물 신고 수리")
+                                .tag("report")
+                                .pathParameters(
+                                        parameterWithName("reportId")
+                                                .description("수정할 신고 ID")
+                                )
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("Bearer (JWT 토큰)")
+                                )
+                                .requestFields(
+                                        fieldWithPath("reportingReason")
+                                                .description("신고 사유")
+                                )
+                                .responseFields(
+                                        fieldWithPath("message")
+                                                .description("성공 메시지 : Ok"),
+                                        fieldWithPath("statusCode")
+                                                .description("성공 상태 코드 : 200"),
+                                        fieldWithPath("data")
+                                                .description("본문 응답"),
+                                        fieldWithPath("data.id")
+                                                .description("신고 ID"),
+                                        fieldWithPath("data.itemId")
+                                                .description("수정된 신고 ID"),
+                                        fieldWithPath("data.reportingReason")
+                                                .description("수정된 신고 사유"),
+                                        fieldWithPath("data.reportStatus")
+                                                .description("신고 상태 : 기본값 WAITING")
+                                )
+                                .requestSchema(Schema.schema("신고-수정-성공-요청"))
+                                .responseSchema(Schema.schema("신고-수정-성공-응답"))
+                                .build()
+                        )));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.data.reportingReason").value(reportingReason));
     }
 
     @Test
-    void deleteReport() {
+    @WithMockUser
+    void deleteReport() throws Exception {
+        //given
+        Long reportId = 1L;
+        doNothing().when(reportService).deleteReport(anyLong(),any(CustomUserDetails.class));
+
+        //when
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.delete("/reports/{reportId}", reportId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer (JWT 토큰)"))
+                .andDo(MockMvcRestDocumentationWrapper.document("update-report",
+                        resource(ResourceSnippetParameters.builder()
+                                .description("신고 ID를 받아서 해당 신고를 삭제합니다.")
+                                .summary("신고 삭제")
+                                .tag("report")
+                                .pathParameters(
+                                        parameterWithName("reportId")
+                                                .description("수정할 신고 ID")
+                                )
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("Bearer (JWT 토큰)")
+                                )
+                                .responseFields(
+                                        fieldWithPath("message")
+                                                .description("성공 메시지 : Ok"),
+                                        fieldWithPath("statusCode")
+                                                .description("성공 상태 코드 : 200"),
+                                        fieldWithPath("data")
+                                                .description("따로 반환하는 본문 없습니다. : null")
+                                )
+                                .responseSchema(Schema.schema("신고-삭제-성공-응답"))
+                                .build()
+                        )));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200));
     }
 }
