61dadc9b8251cb36647336398e52760d0162c22e
EtherXion <etel0103@gmail.com>
2024-10-24 19:14:56 +0900

    임시 확인 용도

--- src/main/java/com/sprarta/sproutmarket/config/SecurityConfig.java
+++ src/main/java/com/sprarta/sproutmarket/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                                 "/test/**",
                                 "/error/**",
                                 "/notifications/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll() // WebSocket 접근 허용
                         .requestMatchers("/admin/**").hasAuthority(UserRole.ADMIN.name())
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/sprarta/sproutmarket/config/WebSocketConfig.java
+++ src/main/java/com/sprarta/sproutmarket/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.sprarta.sproutmarket.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry; // 메시지 브로커 구성 정의
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry; // STOMP 프로토콜의 엔드포인트 등록
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker // 웹소켓 메시징 활성화
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
 
     // 노출할 endpoint 설정
     @Override
@@ -29,4 +32,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 클라이언트->서버로 발행하는 메세지에 대한 endpoint 설정 : 구독에 대한 메세지
         messageBrokerRegistry.setApplicationDestinationPrefixes("/pub"); // 메시지를 발행할 때 사용할 접두사
     }
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(chatPreHandler);
+//    }
+
 }
