84fbc29ada83134ab6c14fcb0779ab5790fd573f
Hyemin Yang <asitwas729@gmail.com>
2024-10-29 16:21:37 +0900

    fix : 검색에 projection 사용

--- src/main/java/com/sprarta/sproutmarket/domain/item/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/com/sprarta/sproutmarket/domain/item/repository/ItemRepositoryCustomImpl.java
@@ -1,9 +1,11 @@
 package com.sprarta.sproutmarket.domain.item.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sprarta.sproutmarket.domain.category.entity.Category;
 import com.sprarta.sproutmarket.domain.common.entity.Status;
+import com.sprarta.sproutmarket.domain.item.dto.response.ItemSearchResponse;
 import com.sprarta.sproutmarket.domain.item.entity.Item;
 import com.sprarta.sproutmarket.domain.item.entity.ItemSaleStatus;
 import com.sprarta.sproutmarket.domain.item.entity.QItem;
@@ -14,44 +16,52 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
     private final JPAQueryFactory queryFactory;
     @Override
-    public Page<Item> searchItems(List<String> areaList, String searchKeyword, Category category, ItemSaleStatus saleStatus, Pageable pageable) {
-        QItem item = QItem.item;
+    public Page<ItemSearchResponse> searchItems(List<String> areaList, String searchKeyword, Category category, ItemSaleStatus saleStatus, Pageable pageable) {
+        QItem qItem = QItem.item;
 
         BooleanBuilder builder = new BooleanBuilder();
 
         // 필수조건(근처 + 활성상태 매물)
-        builder.and(item.seller.address.in(areaList))
-                .and(item.status.eq(Status.ACTIVE));
+        builder.and(qItem.seller.address.in(areaList))
+                .and(qItem.status.eq(Status.ACTIVE));
 
         if(searchKeyword != null && !searchKeyword.isEmpty()){
             String keyword = "%" + searchKeyword + "%";
-            builder.and(item.title.likeIgnoreCase(keyword)
-                .or(item.description.likeIgnoreCase(keyword)));
+            builder.and(qItem.title.likeIgnoreCase(keyword)
+                .or(qItem.description.likeIgnoreCase(keyword)));
         }
         if(category != null) {
-            builder.and(item.category.eq(category));
+            builder.and(qItem.category.eq(category));
         }
         if(saleStatus != null) {
-            builder.and(item.itemSaleStatus.eq(ItemSaleStatus.WAITING));    // 판매중인 매물만 뜨게
+            builder.and(qItem.itemSaleStatus.eq(ItemSaleStatus.WAITING));    // 판매중인 매물만 뜨게
         }
 
         // 전체 개수 가져오기
-        long total = queryFactory.selectFrom(item)
+        long total = queryFactory.selectFrom(qItem)
                                 .where(builder)
                                 .fetchCount();
 
-        // 결과 가져오기(pageable)
-        List<Item> items = queryFactory.selectFrom(item)
-                                        .where(builder)
-                                        .offset(pageable.getOffset())
-                                        .limit(pageable.getPageSize())
-                                        .fetch();
+
+        List<ItemSearchResponse> items = queryFactory.select(Projections.constructor(ItemSearchResponse.class,
+                qItem.id,
+                qItem.title,
+                qItem.price,
+                qItem.seller.address,
+                qItem.images.get(0).name,
+                qItem.createdAt))
+            .from(qItem)
+            .where(builder)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
 
         return new PageImpl<>(items, pageable, total);
     }
