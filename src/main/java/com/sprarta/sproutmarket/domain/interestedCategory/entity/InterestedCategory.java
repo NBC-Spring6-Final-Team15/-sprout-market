35925be9a3c597117723105790efc5e14744f5b0
kikye04040 <kikye04040@gmail.com>
2024-11-03 23:22:39 +0900

    refactor(notification): 엔티티에서 사용하지 않는 부분 정리

--- src/main/java/com/sprarta/sproutmarket/domain/interestedCategory/entity/InterestedCategory.java
+++ src/main/java/com/sprarta/sproutmarket/domain/interestedCategory/entity/InterestedCategory.java
@@ -22,12 +22,7 @@ public class InterestedCategory {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    public InterestedCategory(User user, Category category) {
-        this.user = user;
-        this.category = category;
-    }
-
-    // 연관관계 편의 메서드: User와 Category에 이 엔티티를 추가하는 메서드
+    // 연관관계 편의 메서드: User 와 Category 에 이 엔티티를 추가하는 메서드
     public void setUser(User user) {
         this.user = user;
         // 관심 카테고리가 추가될 때 사용자의 관심 카테고리 목록에도 추가
