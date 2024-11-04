151dd9de02df6c85ce2bf81932ec2b04d27abf6f
durururuk <147094944+durururuk@users.noreply.github.com>
2024-11-04 11:21:17 +0900

    Refactor : 변수 인라인화, 쓰지 않는 변수 선언 제거

--- src/test/java/com/sprarta/sproutmarket/domain/item/service/ItemServiceTest.java
+++ src/test/java/com/sprarta/sproutmarket/domain/item/service/ItemServiceTest.java
@@ -17,7 +17,6 @@ import com.sprarta.sproutmarket.domain.item.dto.request.ItemCreateRequest;
 import com.sprarta.sproutmarket.domain.item.dto.request.ItemSearchRequest;
 import com.sprarta.sproutmarket.domain.item.dto.response.ItemResponse;
 import com.sprarta.sproutmarket.domain.item.dto.response.ItemResponseDto;
-import com.sprarta.sproutmarket.domain.item.dto.response.ItemSearchResponse;
 import com.sprarta.sproutmarket.domain.item.entity.Item;
 import com.sprarta.sproutmarket.domain.item.entity.ItemSaleStatus;
 import com.sprarta.sproutmarket.domain.item.repository.ItemRepository;
@@ -38,7 +37,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
@@ -48,8 +46,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-
-public class ItemServiceTest {
+class ItemServiceTest {
     @Mock
     private ItemRepository itemRepository;
     @Mock
@@ -80,23 +77,18 @@ public class ItemServiceTest {
     private User mockUser;
     private User mockAdmin;
     private Category mockCategory1;
-    private Category mockCategory2;
     private Item mockItem1;
     private Item mockItem2;
     private CustomUserDetails authUser;
     private CustomUserDetails authAdmin;
     private FindItemsInMyAreaRequestDto requestDto;
-    private ItemImage itemImage;
-    private MockMultipartFile mockImage;
 
     @BeforeEach // 코드 실행 전 작동 + 테스트 환경 초기화
     void setup(){
         MockitoAnnotations.openMocks(this); //어노테이션 Mock과 InjectMocks를 초기화
 
-        mockImage = new MockMultipartFile("file", "itemImage.jpg", "image/jpeg", "itemImage content".getBytes());
-
         // 가짜 사용자 생성
-        //String username, String email, String password, String nickname, String phoneNumber, String address, UserRole userRole
+        //String username, String email, String password, String nickname, String phoneNumber, String address
         mockUser = new User(
             "가짜 객체1",
             "mock@mock.com",
@@ -123,7 +115,7 @@ public class ItemServiceTest {
         // 가짜 카테고리 생성
         mockCategory1 = new Category("생활");
         ReflectionTestUtils.setField(mockCategory1, "id", 1L);
-        mockCategory2 = new Category( "가구");
+        Category mockCategory2 = new Category("가구");
         ReflectionTestUtils.setField(mockCategory2, "id", 2L);
 
         // 가짜 매물 생성
@@ -144,7 +136,7 @@ public class ItemServiceTest {
             3000,
             mockUser,
             ItemSaleStatus.WAITING,
-            mockCategory2,
+                mockCategory2,
             Status.ACTIVE
         );
         ReflectionTestUtils.setField(mockItem2, "id", 2L);
@@ -154,11 +146,11 @@ public class ItemServiceTest {
             .size(10)
             .build();
 
-        itemImage = ItemImage.builder()
-            .id(1L)
-            .item(mockItem1)
-            .name("https://sprout-market.s3.ap-northeast-2.amazonaws.com/4da210e1-7.jpg")
-            .build();
+        ItemImage itemImage = ItemImage.builder()
+                .id(1L)
+                .item(mockItem1)
+                .name("https://sprout-market.s3.ap-northeast-2.amazonaws.com/4da210e1-7.jpg")
+                .build();
 
         authUser = new CustomUserDetails(
             mockUser
@@ -222,14 +214,14 @@ public class ItemServiceTest {
             .build();
 
         List<String> areaList = List.of("서울시 관악구 신림동", "서울시 관악구 봉천동");
-        Pageable pageable = PageRequest.of(page - 1, size);
+        Pageable pageable = PageRequest.of(0, size);
 
         when(userRepository.findById(mockUser.getId())).thenReturn(Optional.of(mockUser));
         when(admAreaService.getAdmNameListByAdmName("서울시 관악구 신림동")).thenReturn(areaList);
         when(categoryRepository.findByIdAndStatusIsActiveOrElseThrow(itemSearchRequest.getCategoryId())).thenReturn(mockCategory1);
 
         // When
-        Page<ItemSearchResponse> result = itemService.searchItems(page, size, itemSearchRequest, authUser);
+        itemService.searchItems(page, size, itemSearchRequest, authUser);
 
         // Then
         verify(userRepository, times(1)).findById(authUser.getId());
